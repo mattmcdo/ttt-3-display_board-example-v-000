d9d08892d5e870741bb9be842340c0424b94dc45
Matthew McDonald <matthew.mcd1031@gmail.com>
2019-02-05 03:19:29 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,4 @@
 def display_board
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+  puts "   |   |   "
+  puts "-----------"
 end
-
-display_board
